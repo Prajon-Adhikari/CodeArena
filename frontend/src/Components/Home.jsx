6756c3797b1583897ec029b63ae7e8e6877bc613
Sarthak-Adk <adhikarisarthak33@gmail.com>
2025-04-29 19:37:07 +0545

    home modify

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -8,16 +8,15 @@ export default function home() {
       <nav className="px-22 flex items-center justify-between py-4 bg-white shadow">
         <div className="flex items-center space-x-6">
           <div className="text-2xl font-bold text-blue-900 bg-cyan-500 px-3 py-1 rounded">Hacked</div>
-          <a href="#" className="text-sm text-gray-700 hover:underline">Join a hackathon</a>
-          <a href="#" className="text-sm text-gray-700 hover:underline">Host a hackathon</a>
-          <a href="#" className="text-sm text-gray-700 hover:underline">Resources</a>
+          <Link className="text-sm text-gray-700 hover:underline">Join a hackathon</Link>
+          <Link className="text-sm text-gray-700 hover:underline">Host a hackathon</Link>
+          <Link className="text-sm text-gray-700 hover:underline">Resources</Link>
         </div>
         <div className="flex items-center space-x-4">
           <Link to="/api/auth/signin" className="text-sm text-gray-700">Sign in</Link>
          <Link to="/api/auth/signup" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Sign up</Link>
         </div>
       </nav>
-
       </div>
   );
 }
