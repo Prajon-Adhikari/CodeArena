f9856cc743477b0d0c8128865a81c16208b1af1e
Prajon-Adhikari <prajonadhikari44@gmail.com>
2025-04-30 17:08:43 +0545

    adding interface for home page

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 
 export default function home() {
   const [hackathons, setHackathons] = useState([]);
@@ -48,25 +50,49 @@ export default function home() {
           </Link>
         </div>
       </nav>
-      <div>
+      <div className="grid grid-cols-2 gap-6 py-12 px-[100px]">
         {hackathons.map((hackathon, index) => {
           return (
             <div
               key={hackathon._id}
-              className="mb-4 p-4 bg-white rounded shadow"
+              className="mb-4 p-4 bg-white rounded shadow flex gap-10"
             >
-              <h2 className="text-xl font-semibold">{hackathon.title}</h2>
-              <p>{hackathon.description}</p>
-              <p className="text-sm text-gray-500">
-                Starts: {new Date(hackathon.startDate).toLocaleDateString()}
-              </p>
-              <p className="text-sm text-gray-500">
-                Ends: {new Date(hackathon.endDate).toLocaleDateString()}
-              </p>
-              <p className="text-sm text-gray-500">
-                Registration Deadline:{" "}
-                {new Date(hackathon.registrationDeadline).toLocaleDateString()}
-              </p>
+              <div className="bg-[url('./src/assets/demo-logo.jpg')]  h-30 w-30 bg-cover bg-center">
+                {" "}
+              </div>
+              <div className="w-[500px]">
+                <h2 className="text-3xl font-semibold h-[120px]">
+                  {hackathon.title}
+                </h2>
+                <div className="flex justify-between pr-10 py-4">
+                  <p className="text-lg text-gray-500">
+                    Starts:{" "}
+                    <span className="text-black">
+                      {new Date(hackathon.startDate).toLocaleDateString()}
+                    </span>
+                  </p>
+                  <p className="text-lg text-gray-500">
+                    Ends:{" "}
+                    <span className="text-black">
+                      {new Date(hackathon.endDate).toLocaleDateString()}
+                    </span>
+                  </p>
+                </div>
+                <div className="flex justify-between pr-10 py-4">
+                  <p className="text-lg text-gray-500">
+                    Registration Deadline:{" "}
+                    <span className="text-black">
+                      {new Date(
+                        hackathon.registrationDeadline
+                      ).toLocaleDateString()}
+                    </span>
+                  </p>
+                  <p className="text-lg">
+                    <FontAwesomeIcon icon={faGlobe} className="text-gray-500" />
+                    <span className="ml-2 capitalize">{hackathon.mode}</span>
+                  </p>
+                </div>
+              </div>
             </div>
           );
         })}
