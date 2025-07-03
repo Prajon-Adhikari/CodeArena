1cc618a20df4b2b3e9a7e5f630667ff5a35baf6e
Prajon-Adhikari <prajonadhikari44@gmail.com>
2025-07-03 17:58:47 +0545

    adding design for new page

--- frontend/src/Pages/SpecificHackathon.jsx
+++ frontend/src/Pages/SpecificHackathon.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 export default function SpecificHackathon() {
   const { id } = useParams();
@@ -28,7 +28,32 @@ export default function SpecificHackathon() {
   }, [id]);
   return (
     <div>
-      <h1>{hackathon.title}</h1>
+      <div>
+        {hackathon.bannerUrl ? (
+          <img src={bannerUrl} />
+        ) : (
+          <div className="flex justify-center items-center bg-gray-400 h-[220px] font-semibold text-3xl">
+            {hackathon.title}
+          </div>
+        )}
+        <div className="flex justify-center items-center bg-blue-200 text-xl">
+          <Link to="/overview" className="px-10 py-4 hover:bg-red-200">
+            Overview
+          </Link>
+          <Link to="/myproject" className="px-10 py-4 hover:bg-red-200">
+            My Project
+          </Link>
+          <Link to="/rules" className="px-10 py-4 hover:bg-red-200">
+            Rules
+          </Link>
+          <Link to="/prizes" className="px-10 py-4 hover:bg-red-200">
+            Prizes
+          </Link>
+          <Link to="/judges" className="px-10 py-4 hover:bg-red-200">
+            Judges
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
