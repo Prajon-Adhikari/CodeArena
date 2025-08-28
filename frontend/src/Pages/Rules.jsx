023c887cc64e765bcb48f22edfea541328ec77fb
Prajon-Adhikari <prajonadhikari44@gmail.com>
2025-08-28 14:33:51 +0545

    managing rules data in frontend

--- frontend/src/Pages/Rules.jsx
+++ frontend/src/Pages/Rules.jsx
@@ -1,5 +1,12 @@
 import { useState, useEffect } from "react";
 import { Link, useParams, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGlobe,
+  faBuildingColumns,
+  faFlag,
+  faTags,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Rules() {
   const { id } = useParams();
@@ -14,6 +21,12 @@ export default function Rules() {
     { path: "prizes", label: "Prizes" },
     { path: "judges", label: "Judges" },
   ];
+
+  const ruleStyle = {
+    marginBottom: "15px",
+    whiteSpace: "pre-line", // <- preserves \n as line breaks
+    paddingLeft: "8px",
+  };
 
   useEffect(() => {
     const fetchHackathonDetails = async () => {
@@ -40,7 +53,6 @@ export default function Rules() {
   useEffect(() => {
     const fetchHackathonRules = async () => {
       try {
-        console.log("hello");
         const response = await fetch(
           `${import.meta.env.VITE_API_BASE_URL}/home/${id}/rules`,
           {
@@ -52,8 +64,7 @@ export default function Rules() {
           }
         );
         const data = await response.json();
-        console.log("heelo");
-        console.log(data.rules);
+        setHackathonRules(data.rules);
       } catch (error) {
         console.log("Error while fetching hackathon rules", error);
       }
@@ -86,8 +97,166 @@ export default function Rules() {
           ))}
         </div>
       </div>
-      <div className="px-[150px] py-20 ">
-        <h2 className="text-3xl font-bold">Rules</h2>
+      <div className="flex px-[200px] py-20  gap-20">
+        <div className="">
+          <h2 className="text-5xl font-bold mb-10 pb-1 px-2 inline-block border-b-3 border-b-gray-700">
+            Rules
+          </h2>
+
+          {hackathonRules.eligibility && (
+            <div style={ruleStyle}>
+              <h3 className="text-2xl font-bold pb-2">Eligibility</h3>
+              {hackathonRules.eligibility.split("\n").map((line, index) => (
+                <p key={index} className="text-xl mb-4">
+                  {line}
+                </p>
+              ))}
+            </div>
+          )}
+
+          {hackathonRules.teamFormation && (
+            <div style={ruleStyle}>
+              <h3 className="text-2xl font-bold pb-2 pt-6">Team Formation</h3>
+              {hackathonRules.teamFormation.split("\n").map((line, index) => (
+                <p key={index} className="text-xl mb-4">
+                  {line}
+                </p>
+              ))}
+            </div>
+          )}
+
+          {hackathonRules.submissionRequirements && (
+            <div style={ruleStyle}>
+              <h3 className="text-2xl font-bold pb-2 pt-6">
+                Submission Requirements
+              </h3>
+              {hackathonRules.submissionRequirements
+                .split("\n")
+                .map((line, index) => (
+                  <p key={index} className="text-xl mb-4">
+                    {line}
+                  </p>
+                ))}
+            </div>
+          )}
+
+          {hackathonRules.codeOfConduct && (
+            <div style={ruleStyle}>
+              <h3 className="text-2xl font-bold pb-2 pt-6">Code of Conduct</h3>
+              {hackathonRules.codeOfConduct.split("\n").map((line, index) => (
+                <p key={index} className="text-xl mb-4">
+                  {line}
+                </p>
+              ))}
+            </div>
+          )}
+
+          {hackathonRules.prohibited && (
+            <div style={ruleStyle}>
+              <h3 className="text-2xl font-bold pb-2 pt-6">Prohibited</h3>
+              {hackathonRules.prohibited.split("\n").map((line, index) => (
+                <p key={index} className="text-xl mb-4">
+                  {line}
+                </p>
+              ))}
+            </div>
+          )}
+
+          {hackathonRules.disqualification && (
+            <div style={ruleStyle}>
+              <h3 className="text-2xl font-bold pb-2 pt-6">Disqualification</h3>
+              {hackathonRules.disqualification
+                .split("\n")
+                .map((line, index) => (
+                  <p key={index} className="text-xl mb-4">
+                    {line}
+                  </p>
+                ))}
+            </div>
+          )}
+        </div>
+        <div>
+          <div className="bg-[#F8F8F8] border-1 border-gray-400 p-6  w-[400px] rounded-lg">
+            <p className="bg-orange-200 w-45 text-center py-1 rounded-3xl">
+              {" "}
+              {(() => {
+                const deadline = new Date(hackathon.registrationDeadline);
+                const today = new Date();
+                const timeDiff = deadline.getTime() - today.getTime();
+                const remainingDays = Math.ceil(
+                  timeDiff / (1000 * 60 * 60 * 24)
+                );
+
+                return remainingDays > 0
+                  ? `${remainingDays} days to deadline`
+                  : "Deadline passed";
+              })()}
+            </p>
+            <p className="text-lg font-bold mt-2 pl-2">Registraion Deadline</p>
+            <p className="pl-2 pt-1 pb-4 border-b-1 border-gray-400">
+              {" "}
+              {(() => {
+                const deadline = new Date(hackathon.registrationDeadline);
+                const formattedDate = deadline.toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
+                });
+
+                return formattedDate;
+              })()}
+            </p>
+            <div className="flex justify-between px-4 py-8 text-lg border-b-1 border-gray-400">
+              <div className="flex flex-col gap-5">
+                <p>
+                  <FontAwesomeIcon icon={faGlobe} className="mr-2" />
+                  {hackathon.mode
+                    ? hackathon.mode.charAt(0).toUpperCase() +
+                      hackathon.mode.slice(1)
+                    : ""}
+                </p>
+                <p>
+                  <span className="font-bold">$ 100000</span> in cash
+                </p>
+              </div>
+              <div className="flex flex-col gap-5">
+                <p>
+                  <FontAwesomeIcon icon={faBuildingColumns} className="mr-2" />
+                  Public
+                </p>
+                <p>
+                  <span className="font-bold">923</span> participants
+                </p>
+              </div>
+            </div>
+            <div className="my-8 mx-5">
+              <div className="flex items-center justify-start gap-5 text-lg mb-5">
+                <FontAwesomeIcon
+                  icon={faFlag}
+                  className="text-slate-700 text-xl"
+                />
+                <span>Google</span>
+              </div>
+              <div className="flex justify-start gap-5 text-sm">
+                <FontAwesomeIcon
+                  icon={faTags}
+                  className="text-slate-700 text-xl pt-1"
+                />
+                <span>
+                  <div className="bg-orange-200 mb-1 mr-1 inline-block  px-4 py-1">
+                    Machine Learning
+                  </div>
+                  <div className="bg-orange-200 mb-1 mr-1 inline-block  px-4 py-1">
+                    IoT
+                  </div>
+                  <div className="bg-orange-200 mb-1 mr-1 inline-block  px-4 py-1">
+                    Beginner Friendly
+                  </div>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
