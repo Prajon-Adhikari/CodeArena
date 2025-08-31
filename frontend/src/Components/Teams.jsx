32e9ad252fd6e6bf842fe5466aafd7482dc5455d
Prajon-Adhikari <prajonadhikari44@gmail.com>
2025-08-31 14:21:36 +0545

    adding social media for each team members

--- frontend/src/Components/Teams.jsx
+++ frontend/src/Components/Teams.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook, faInstagram,
+  faGithub,
+  faLinkedinIn
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Teams({image, name, role}) {
   return (
@@ -6,7 +12,12 @@ export default function Teams({image, name, role}) {
       <img src={image} alt="" className="w-[100px] h-[100px] object-cover rounded-full mb-4"/>
       <p className="font-bold text-xl">{name}</p>
       <p className="text-gray-600">{role}</p>
-      <div></div>
+      <div className="flex gap-5 pt-2">
+        <FontAwesomeIcon icon={faFacebook}  className="bg-gray-600 text-white text-lg p-[6px] rounded-md"/>
+        <FontAwesomeIcon icon={faLinkedinIn} className="bg-gray-600 text-white text-lg p-[6px] rounded-md"/>
+        <FontAwesomeIcon icon={faGithub} className="bg-gray-600 text-white text-lg p-[6px] rounded-md"/>
+        <FontAwesomeIcon icon={faInstagram} className="bg-gray-600 text-white text-lg p-[6px] rounded-md"/>
+      </div>
     </div>
   );
 }
