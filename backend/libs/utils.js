51572141a045e84de0f1e00851551bf79213b0aa
Prajon-Adhikari <prajonadhikari44@gmail.com>
2025-11-22 16:33:44 +0545

    handling token error

--- backend/libs/utils.js
+++ backend/libs/utils.js
@@ -8,8 +8,8 @@ export const generateToken = (user, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "Lax",
-    secure: false,
+    sameSite: "none",
+    secure: true,
   });
 
   return token;
